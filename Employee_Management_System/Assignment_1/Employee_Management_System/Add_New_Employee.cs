ef901847a7607d149995bc9ae69d759aecfe0c00
snehal renushe <snehalrenushe15@gmail.com>
2021-07-08 22:10:46 +0530

    Login Form,Accept and Display Data in Grid and Add Database

---
+++ Employee_Management_System/Assignment_1/Employee_Management_System/Add_New_Employee.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Employee_Management_System
+{
+    public partial class frm_Add_New_Employee : Form
+    {
+        public frm_Add_New_Employee()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection Con = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=DB_Employee_Management_System;Integrated Security=True");
+
+        void Con_Open()
+        {
+            if (Con.State == ConnectionState.Closed)
+            {
+                Con.Open();
+            }
+        }
+
+        void Con_Close()
+        {
+            if (Con.State == ConnectionState.Open)
+            {
+                Con.Close();
+            }
+        }
+
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            if (tb_Emp_ID.Text != "" && tb_Name.Text != "" && tb_Post.Text != "")
+            {
+                Con_Open();
+
+                SqlCommand Cmd = new SqlCommand();
+
+                Cmd.Connection = Con;
+
+                Cmd.CommandText = " INSERT INTO Employee_Details (Emp_ID, Name, DOB, Post) VALUES (@Emp_ID,@Name,@Date_Of_Birth,@Post) ";
+
+                Cmd.Parameters.Add("Emp_ID", SqlDbType.Int).Value = tb_Emp_ID.Text;
+                Cmd.Parameters.Add("Name", SqlDbType.NVarChar).Value = tb_Name.Text;
+                Cmd.Parameters.Add("Date_Of_Birth", SqlDbType.Date).Value = dtp_Date_Of_Birth.Text;
+                Cmd.Parameters.Add("Post", SqlDbType.NVarChar).Value = tb_Post.Text;
+
+                Cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Record Saved Sucessfully!!!","Saving",MessageBoxButtons.OK,MessageBoxIcon.Information);
+
+                tb_Emp_ID.Clear();
+                tb_Name.Clear();
+                tb_Post.Clear();
+                dtp_Date_Of_Birth.ResetText();
+
+                Con_Close();
+            }
+            else
+            {
+                MessageBox.Show("Incomplete Information..");
+            }
+        }
+
+        private void btn_View_All_Employee_Click(object sender, EventArgs e)
+        {
+            frm_View_All_Employee VAE = new frm_View_All_Employee();
+
+            VAE.Show();
+
+            this.Hide();
+        }
+
+        private void btn_Log_Out_Click(object sender, EventArgs e)
+        {
+            DialogResult Res = MessageBox.Show("Logging Out","Are you Sure?" , MessageBoxButtons.YesNo , MessageBoxIcon.Question);
+
+            if (Res == DialogResult.Yes)
+            {
+                frm_Login_Form Lf = new frm_Login_Form();
+
+                Lf.Show();
+
+                this.Hide();
+            }
+        }
+
+
+    }
+}
---
+++ Employee_Management_System/Assignment_1/Employee_Management_System/Login.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Employee_Management_System
+{
+    partial class frm_Login_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Header = new System.Windows.Forms.Label();
+            this.lbl_Username = new System.Windows.Forms.Label();
+            this.lbl_Password = new System.Windows.Forms.Label();
+            this.tb_Username = new System.Windows.Forms.TextBox();
+            this.tb_Password = new System.Windows.Forms.TextBox();
+            this.btn_Submit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_Header
+            // 
+            this.lbl_Header.AutoSize = true;
+            this.lbl_Header.BackColor = System.Drawing.Color.White;
+            this.lbl_Header.Font = new System.Drawing.Font("Cooper Black", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Header.ForeColor = System.Drawing.Color.Red;
+            this.lbl_Header.Location = new System.Drawing.Point(294, 36);
+            this.lbl_Header.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_Header.Name = "lbl_Header";
+            this.lbl_Header.Size = new System.Drawing.Size(381, 69);
+            this.lbl_Header.TabIndex = 0;
+            this.lbl_Header.Text = "Login Form";
+            // 
+            // lbl_Username
+            // 
+            this.lbl_Username.AutoSize = true;
+            this.lbl_Username.Font = new System.Drawing.Font("Cooper Black", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Username.Location = new System.Drawing.Point(130, 154);
+            this.lbl_Username.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_Username.Name = "lbl_Username";
+            this.lbl_Username.Size = new System.Drawing.Size(224, 46);
+            this.lbl_Username.TabIndex = 1;
+            this.lbl_Username.Text = "Username";
+            // 
+            // lbl_Password
+            // 
+            this.lbl_Password.AutoSize = true;
+            this.lbl_Password.Font = new System.Drawing.Font("Cooper Black", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Password.Location = new System.Drawing.Point(130, 244);
+            this.lbl_Password.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(219, 46);
+            this.lbl_Password.TabIndex = 2;
+            this.lbl_Password.Text = "Password";
+            // 
+            // tb_Username
+            // 
+            this.tb_Username.Font = new System.Drawing.Font("Cooper Black", 15F);
+            this.tb_Username.Location = new System.Drawing.Point(505, 154);
+            this.tb_Username.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_Username.MaxLength = 12;
+            this.tb_Username.Multiline = true;
+            this.tb_Username.Name = "tb_Username";
+            this.tb_Username.Size = new System.Drawing.Size(246, 31);
+            this.tb_Username.TabIndex = 3;
+            this.tb_Username.TextChanged += new System.EventHandler(this.tb_Username_TextChanged);
+            // 
+            // tb_Password
+            // 
+            this.tb_Password.Enabled = false;
+            this.tb_Password.Font = new System.Drawing.Font("Cooper Black", 15F);
+            this.tb_Password.Location = new System.Drawing.Point(505, 244);
+            this.tb_Password.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_Password.MaxLength = 10;
+            this.tb_Password.Multiline = true;
+            this.tb_Password.Name = "tb_Password";
+            this.tb_Password.PasswordChar = '*';
+            this.tb_Password.Size = new System.Drawing.Size(246, 33);
+            this.tb_Password.TabIndex = 4;
+            this.tb_Password.TextChanged += new System.EventHandler(this.tb_Password_TextChanged);
+            // 
+            // btn_Submit
+            // 
+            this.btn_Submit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn_Submit.Font = new System.Drawing.Font("Algerian", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Submit.ForeColor = System.Drawing.Color.Crimson;
+            this.btn_Submit.Location = new System.Drawing.Point(342, 353);
+            this.btn_Submit.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_Submit.Name = "btn_Submit";
+            this.btn_Submit.Size = new System.Drawing.Size(156, 37);
+            this.btn_Submit.TabIndex = 5;
+            this.btn_Submit.Text = "Submit";
+            this.btn_Submit.UseVisualStyleBackColor = false;
+            this.btn_Submit.Click += new System.EventHandler(this.btn_Submit_Click);
+            // 
+            // frm_Login_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(906, 466);
+            this.Controls.Add(this.btn_Submit);
+            this.Controls.Add(this.tb_Password);
+            this.Controls.Add(this.tb_Username);
+            this.Controls.Add(this.lbl_Password);
+            this.Controls.Add(this.lbl_Username);
+            this.Controls.Add(this.lbl_Header);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Purple;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "frm_Login_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Header;
+        private System.Windows.Forms.Label lbl_Username;
+        private System.Windows.Forms.Label lbl_Password;
+        private System.Windows.Forms.TextBox tb_Username;
+        private System.Windows.Forms.TextBox tb_Password;
+        private System.Windows.Forms.Button btn_Submit;
+    }
+}
+
