b2ba7ac2460ad0b36c4f06ab57b296d9e0c540a4
snehal renushe <snehalrenushe15@gmail.com>
2022-02-13 23:23:02 +0530

    CRUD Application using Stored Procedures

---
+++ CRUD_Using_Stored_Procedure/CRUD_Using_Stored_Procedure/CRUD_Application.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace CRUD_Using_Stored_Procedure
+{
+    partial class CRUD_Application
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Crud_Application = new System.Windows.Forms.Label();
+            this.lbl_Student_ID = new System.Windows.Forms.Label();
+            this.tb_Student_ID = new System.Windows.Forms.TextBox();
+            this.lbl_Name = new System.Windows.Forms.Label();
+            this.tb_Name = new System.Windows.Forms.TextBox();
+            this.tb_Address = new System.Windows.Forms.TextBox();
+            this.tb_Mobile_No = new System.Windows.Forms.TextBox();
+            this.gb_Gender = new System.Windows.Forms.GroupBox();
+            this.rb_Female = new System.Windows.Forms.RadioButton();
+            this.rb_Male = new System.Windows.Forms.RadioButton();
+            this.lbl_Mobile_No = new System.Windows.Forms.Label();
+            this.lbl_Gender = new System.Windows.Forms.Label();
+            this.lbl_Address = new System.Windows.Forms.Label();
+            this.lbl_Date_Of_Birth = new System.Windows.Forms.Label();
+            this.dtp_Date_Of_Birth = new System.Windows.Forms.DateTimePicker();
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.btn_Update = new System.Windows.Forms.Button();
+            this.dgv_Crud_Application = new System.Windows.Forms.DataGridView();
+            this.btn_Refresh = new System.Windows.Forms.Button();
+            this.gb_Gender.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Crud_Application)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_Crud_Application
+            // 
+            this.lbl_Crud_Application.AutoSize = true;
+            this.lbl_Crud_Application.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lbl_Crud_Application.Font = new System.Drawing.Font("Modern No. 20", 40F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Crud_Application.ForeColor = System.Drawing.Color.Indigo;
+            this.lbl_Crud_Application.Location = new System.Drawing.Point(549, 35);
+            this.lbl_Crud_Application.Name = "lbl_Crud_Application";
+            this.lbl_Crud_Application.Size = new System.Drawing.Size(818, 83);
+            this.lbl_Crud_Application.TabIndex = 0;
+            this.lbl_Crud_Application.Text = "CRUD APPLICATION";
+            // 
+            // lbl_Student_ID
+            // 
+            this.lbl_Student_ID.AutoSize = true;
+            this.lbl_Student_ID.Font = new System.Drawing.Font("Rockwell", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Student_ID.ForeColor = System.Drawing.Color.Red;
+            this.lbl_Student_ID.Location = new System.Drawing.Point(545, 132);
+            this.lbl_Student_ID.Name = "lbl_Student_ID";
+            this.lbl_Student_ID.Size = new System.Drawing.Size(208, 44);
+            this.lbl_Student_ID.TabIndex = 1;
+            this.lbl_Student_ID.Text = "Student ID";
+            // 
+            // tb_Student_ID
+            // 
+            this.tb_Student_ID.Font = new System.Drawing.Font("Rockwell", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Student_ID.Location = new System.Drawing.Point(1027, 132);
+            this.tb_Student_ID.MaxLength = 3;
+            this.tb_Student_ID.Multiline = true;
+            this.tb_Student_ID.Name = "tb_Student_ID";
+            this.tb_Student_ID.Size = new System.Drawing.Size(330, 44);
+            this.tb_Student_ID.TabIndex = 1;
+            // 
+            // lbl_Name
+            // 
+            this.lbl_Name.AutoSize = true;
+            this.lbl_Name.Font = new System.Drawing.Font("Rockwell", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Name.ForeColor = System.Drawing.Color.Red;
+            this.lbl_Name.Location = new System.Drawing.Point(545, 208);
+            this.lbl_Name.Name = "lbl_Name";
+            this.lbl_Name.Size = new System.Drawing.Size(127, 44);
+            this.lbl_Name.TabIndex = 0;
+            this.lbl_Name.Text = "Name";
+            // 
+            // tb_Name
+            // 
+            this.tb_Name.Font = new System.Drawing.Font("Rockwell", 14F, System.Drawing.FontStyle.Bold);
+            this.tb_Name.Location = new System.Drawing.Point(1027, 208);
+            this.tb_Name.MaxLength = 30;
+            this.tb_Name.Multiline = true;
+            this.tb_Name.Name = "tb_Name";
+            this.tb_Name.Size = new System.Drawing.Size(330, 44);
+            this.tb_Name.TabIndex = 2;
+            // 
+            // tb_Address
+            // 
+            this.tb_Address.Font = new System.Drawing.Font("Rockwell", 14F, System.Drawing.FontStyle.Bold);
+            this.tb_Address.Location = new System.Drawing.Point(1027, 347);
+            this.tb_Address.MaxLength = 30;
+            this.tb_Address.Multiline = true;
+            this.tb_Address.Name = "tb_Address";
+            this.tb_Address.Size = new System.Drawing.Size(330, 44);
+            this.tb_Address.TabIndex = 4;
+            // 
+            // tb_Mobile_No
+            // 
+            this.tb_Mobile_No.Font = new System.Drawing.Font("Rockwell", 14F, System.Drawing.FontStyle.Bold);
+            this.tb_Mobile_No.Location = new System.Drawing.Point(1027, 495);
+            this.tb_Mobile_No.MaxLength = 10;
+            this.tb_Mobile_No.Multiline = true;
+            this.tb_Mobile_No.Name = "tb_Mobile_No";
+            this.tb_Mobile_No.Size = new System.Drawing.Size(330, 44);
+            this.tb_Mobile_No.TabIndex = 6;
+            // 
+            // gb_Gender
+            // 
+            this.gb_Gender.Controls.Add(this.rb_Male);
+            this.gb_Gender.Controls.Add(this.rb_Female);
+            this.gb_Gender.Font = new System.Drawing.Font("Rockwell", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gb_Gender.Location = new System.Drawing.Point(1027, 410);
+            this.gb_Gender.Name = "gb_Gender";
+            this.gb_Gender.Size = new System.Drawing.Size(330, 56);
+            this.gb_Gender.TabIndex = 5;
+            this.gb_Gender.TabStop = false;
+            // 
+            // rb_Female
+            // 
+            this.rb_Female.AutoSize = true;
+            this.rb_Female.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb_Female.Location = new System.Drawing.Point(19, 17);
+            this.rb_Female.Name = "rb_Female";
+            this.rb_Female.Size = new System.Drawing.Size(128, 33);
+            this.rb_Female.TabIndex = 0;
+            this.rb_Female.TabStop = true;
+            this.rb_Female.Text = "Female";
+            this.rb_Female.UseVisualStyleBackColor = true;
+            // 
+            // rb_Male
+            // 
+            this.rb_Male.AutoSize = true;
+            this.rb_Male.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold);
+            this.rb_Male.Location = new System.Drawing.Point(195, 17);
+            this.rb_Male.Name = "rb_Male";
+            this.rb_Male.Size = new System.Drawing.Size(99, 33);
+            this.rb_Male.TabIndex = 0;
+            this.rb_Male.TabStop = true;
+            this.rb_Male.Text = "Male";
+            this.rb_Male.UseVisualStyleBackColor = true;
+            // 
+            // lbl_Mobile_No
+            // 
+            this.lbl_Mobile_No.AutoSize = true;
+            this.lbl_Mobile_No.Font = new System.Drawing.Font("Rockwell", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Mobile_No.ForeColor = System.Drawing.Color.Red;
+            this.lbl_Mobile_No.Location = new System.Drawing.Point(545, 495);
+            this.lbl_Mobile_No.Name = "lbl_Mobile_No";
+            this.lbl_Mobile_No.Size = new System.Drawing.Size(208, 44);
+            this.lbl_Mobile_No.TabIndex = 9;
+            this.lbl_Mobile_No.Text = "Mobile No";
+            // 
+            // lbl_Gender
+            // 
+            this.lbl_Gender.AutoSize = true;
+            this.lbl_Gender.Font = new System.Drawing.Font("Rockwell", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Gender.ForeColor = System.Drawing.Color.Red;
+            this.lbl_Gender.Location = new System.Drawing.Point(545, 422);
+            this.lbl_Gender.Name = "lbl_Gender";
+            this.lbl_Gender.Size = new System.Drawing.Size(154, 44);
+            this.lbl_Gender.TabIndex = 11;
+            this.lbl_Gender.Text = "Gender";
+            // 
+            // lbl_Address
+            // 
+            this.lbl_Address.AutoSize = true;
+            this.lbl_Address.Font = new System.Drawing.Font("Rockwell", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Address.ForeColor = System.Drawing.Color.Red;
+            this.lbl_Address.Location = new System.Drawing.Point(545, 347);
+            this.lbl_Address.Name = "lbl_Address";
+            this.lbl_Address.Size = new System.Drawing.Size(164, 44);
+            this.lbl_Address.TabIndex = 12;
+            this.lbl_Address.Text = "Address";
+            // 
+            // lbl_Date_Of_Birth
+            // 
+            this.lbl_Date_Of_Birth.AutoSize = true;
+            this.lbl_Date_Of_Birth.Font = new System.Drawing.Font("Rockwell", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Date_Of_Birth.ForeColor = System.Drawing.Color.Red;
+            this.lbl_Date_Of_Birth.Location = new System.Drawing.Point(545, 280);
+            this.lbl_Date_Of_Birth.Name = "lbl_Date_Of_Birth";
+            this.lbl_Date_Of_Birth.Size = new System.Drawing.Size(256, 44);
+            this.lbl_Date_Of_Birth.TabIndex = 14;
+            this.lbl_Date_Of_Birth.Text = "Date Of Birth";
+            // 
+            // dtp_Date_Of_Birth
+            // 
+            this.dtp_Date_Of_Birth.Font = new System.Drawing.Font("Rockwell", 14F, System.Drawing.FontStyle.Bold);
+            this.dtp_Date_Of_Birth.Location = new System.Drawing.Point(1027, 282);
+            this.dtp_Date_Of_Birth.Name = "dtp_Date_Of_Birth";
+            this.dtp_Date_Of_Birth.Size = new System.Drawing.Size(330, 40);
+            this.dtp_Date_Of_Birth.TabIndex = 3;
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.BackColor = System.Drawing.Color.Yellow;
+            this.btn_Add.Font = new System.Drawing.Font("MV Boli", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Add.ForeColor = System.Drawing.Color.Black;
+            this.btn_Add.Location = new System.Drawing.Point(551, 572);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(155, 63);
+            this.btn_Add.TabIndex = 15;
+            this.btn_Add.Text = "Add";
+            this.btn_Add.UseVisualStyleBackColor = false;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.BackColor = System.Drawing.Color.Yellow;
+            this.btn_Search.Font = new System.Drawing.Font("MV Boli", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Search.ForeColor = System.Drawing.Color.Black;
+            this.btn_Search.Location = new System.Drawing.Point(1409, 125);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(155, 61);
+            this.btn_Search.TabIndex = 16;
+            this.btn_Search.Text = "Search";
+            this.btn_Search.UseVisualStyleBackColor = false;
+            this.btn_Search.Click += new System.EventHandler(this.btn_Search_Click);
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.BackColor = System.Drawing.Color.Yellow;
+            this.btn_Delete.Font = new System.Drawing.Font("MV Boli", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Delete.ForeColor = System.Drawing.Color.Black;
+            this.btn_Delete.Location = new System.Drawing.Point(1001, 572);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(155, 63);
+            this.btn_Delete.TabIndex = 17;
+            this.btn_Delete.Text = "Delete";
+            this.btn_Delete.UseVisualStyleBackColor = false;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
+            // 
+            // btn_Update
+            // 
+            this.btn_Update.BackColor = System.Drawing.Color.Yellow;
+            this.btn_Update.Font = new System.Drawing.Font("MV Boli", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Update.ForeColor = System.Drawing.Color.Black;
+            this.btn_Update.Location = new System.Drawing.Point(781, 572);
+            this.btn_Update.Name = "btn_Update";
+            this.btn_Update.Size = new System.Drawing.Size(155, 63);
+            this.btn_Update.TabIndex = 18;
+            this.btn_Update.Text = "Update";
+            this.btn_Update.UseVisualStyleBackColor = false;
+            this.btn_Update.Click += new System.EventHandler(this.btn_Update_Click);
+            // 
+            // dgv_Crud_Application
+            // 
+            this.dgv_Crud_Application.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_Crud_Application.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.dgv_Crud_Application.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_Crud_Application.Location = new System.Drawing.Point(290, 655);
+            this.dgv_Crud_Application.Name = "dgv_Crud_Application";
+            this.dgv_Crud_Application.RowTemplate.Height = 28;
+            this.dgv_Crud_Application.Size = new System.Drawing.Size(1398, 342);
+            this.dgv_Crud_Application.TabIndex = 19;
+            // 
+            // btn_Refresh
+            // 
+            this.btn_Refresh.BackColor = System.Drawing.Color.Yellow;
+            this.btn_Refresh.Font = new System.Drawing.Font("MV Boli", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Refresh.ForeColor = System.Drawing.Color.Black;
+            this.btn_Refresh.Location = new System.Drawing.Point(1202, 572);
+            this.btn_Refresh.Name = "btn_Refresh";
+            this.btn_Refresh.Size = new System.Drawing.Size(155, 63);
+            this.btn_Refresh.TabIndex = 20;
+            this.btn_Refresh.Text = "Refresh";
+            this.btn_Refresh.UseVisualStyleBackColor = false;
+            this.btn_Refresh.Click += new System.EventHandler(this.btn_Refresh_Click);
+            // 
+            // CRUD_Application
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MintCream;
+            this.ClientSize = new System.Drawing.Size(1709, 1021);
+            this.Controls.Add(this.btn_Refresh);
+            this.Controls.Add(this.dgv_Crud_Application);
+            this.Controls.Add(this.btn_Update);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.btn_Search);
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.lbl_Date_Of_Birth);
+            this.Controls.Add(this.dtp_Date_Of_Birth);
+            this.Controls.Add(this.lbl_Address);
+            this.Controls.Add(this.lbl_Gender);
+            this.Controls.Add(this.lbl_Mobile_No);
+            this.Controls.Add(this.gb_Gender);
+            this.Controls.Add(this.tb_Mobile_No);
+            this.Controls.Add(this.tb_Address);
+            this.Controls.Add(this.tb_Name);
+            this.Controls.Add(this.lbl_Name);
+            this.Controls.Add(this.tb_Student_ID);
+            this.Controls.Add(this.lbl_Student_ID);
+            this.Controls.Add(this.lbl_Crud_Application);
+            this.Name = "CRUD_Application";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CRUD Application";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.CRUD_Application_Load);
+            this.gb_Gender.ResumeLayout(false);
+            this.gb_Gender.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Crud_Application)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Crud_Application;
+        private System.Windows.Forms.Label lbl_Student_ID;
+        private System.Windows.Forms.TextBox tb_Student_ID;
+        private System.Windows.Forms.Label lbl_Name;
+        private System.Windows.Forms.TextBox tb_Name;
+        private System.Windows.Forms.TextBox tb_Address;
+        private System.Windows.Forms.TextBox tb_Mobile_No;
+        private System.Windows.Forms.GroupBox gb_Gender;
+        private System.Windows.Forms.RadioButton rb_Male;
+        private System.Windows.Forms.RadioButton rb_Female;
+        private System.Windows.Forms.Label lbl_Mobile_No;
+        private System.Windows.Forms.Label lbl_Gender;
+        private System.Windows.Forms.Label lbl_Address;
+        private System.Windows.Forms.Label lbl_Date_Of_Birth;
+        private System.Windows.Forms.DateTimePicker dtp_Date_Of_Birth;
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.Button btn_Update;
+        private System.Windows.Forms.DataGridView dgv_Crud_Application;
+        private System.Windows.Forms.Button btn_Refresh;
+    }
+}
+
---
+++ CRUD_Using_Stored_Procedure/CRUD_Using_Stored_Procedure/CRUD_Application.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CRUD_Using_Stored_Procedure
+{
+    public partial class CRUD_Application : Form
+    {
+        public CRUD_Application()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection Con = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=DB_CRUD_Using_Stored_Procedure;Integrated Security=True");
+
+        void Con_Open()
+        {
+            if (Con.State == ConnectionState.Closed)
+            {
+                Con.Open();   
+            }
+        }
+
+        void Con_Close()
+        {
+            if (Con.State == ConnectionState.Open)
+            {
+                Con.Close();
+            }
+        }
+
+        void Clear_Controls()
+        {
+            tb_Student_ID.Clear();
+            tb_Name.Clear();
+            tb_Address.Clear();
+            tb_Mobile_No.Clear();
+            rb_Female.Checked = false;
+            rb_Male.Checked = false;
+        }
+
+        private void CRUD_Application_Load(object sender, EventArgs e)
+        {
+            View_Student_Details();
+        }
+
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            string gen = " ";
+
+            if (tb_Student_ID.Text != "" && tb_Name.Text != "" && tb_Address.Text != "" && tb_Mobile_No.Text != "")
+            {
+                Con_Open();
+
+                if (rb_Female.Checked)
+                {
+                    gen = rb_Female.Text;
+                }
+                else
+                {
+                    gen = rb_Male.Text;
+                }
+
+                SqlCommand Cmd = new SqlCommand("Insert_Student",Con);
+
+                Cmd.CommandType = CommandType.StoredProcedure;
+
+                Cmd.Parameters.Add("@ID",SqlDbType.Int).Value = tb_Student_ID.Text;
+                Cmd.Parameters.Add("@Name",SqlDbType.NVarChar).Value = tb_Name.Text;
+                Cmd.Parameters.Add("@DOB",SqlDbType.NVarChar).Value = dtp_Date_Of_Birth.Text;
+                Cmd.Parameters.Add("@Add",SqlDbType.NVarChar).Value = tb_Address.Text;
+                Cmd.Parameters.Add("@Gen",SqlDbType.NVarChar).Value = gen;
+                Cmd.Parameters.Add("@Mob",SqlDbType.Decimal).Value = tb_Mobile_No.Text;
+
+                Cmd.Connection = Con;
+
+                Cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Student Details Saved Successfully !!!", "Saving", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                View_Student_Details();
+                
+                Clear_Controls();
+
+                Con_Close();
+            }
+            else
+            {
+                MessageBox.Show("Insert Valid Details !!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            
+        }
+
+        void View_Student_Details()
+        {
+            SqlCommand Cmd = new SqlCommand("View_Student",Con);
+
+            SqlDataAdapter SDA = new SqlDataAdapter(Cmd);
+
+            DataTable dt = new DataTable();
+
+            SDA.Fill(dt);
+
+            dgv_Crud_Application.DataSource = dt;
+        }
+
+        private void btn_Refresh_Click(object sender, EventArgs e)
+        {
+            Clear_Controls();
+            View_Student_Details();
+        }
+
+        private void btn_Update_Click(object sender, EventArgs e)
+        {
+            string gen = " ";
+
+            if (tb_Student_ID.Text != "" && tb_Name.Text != "" && tb_Address.Text != "" && tb_Mobile_No.Text != "")
+            {
+                Con_Open();
+
+                if (rb_Female.Checked)
+                {
+                    gen = rb_Female.Text;
+                }
+                else
+                {
+                    gen = rb_Male.Text;
+                }
+
+                SqlCommand Cmd = new SqlCommand("Update_Student", Con);
+
+                Cmd.CommandType = CommandType.StoredProcedure;
+
+                Cmd.Parameters.Add("@ID", SqlDbType.Int).Value = tb_Student_ID.Text;
+                Cmd.Parameters.Add("@Name", SqlDbType.NVarChar).Value = tb_Name.Text;
+                Cmd.Parameters.Add("@DOB", SqlDbType.NVarChar).Value = dtp_Date_Of_Birth.Text;
+                Cmd.Parameters.Add("@Add", SqlDbType.NVarChar).Value = tb_Address.Text;
+                Cmd.Parameters.Add("@Gen", SqlDbType.NVarChar).Value = gen;
+                Cmd.Parameters.Add("@Mob", SqlDbType.Decimal).Value = tb_Mobile_No.Text;
+
+                Cmd.Connection = Con;
+
+                Cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Student Record Updated Successfully !!!", "Saving", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                View_Student_Details();
+
+                Clear_Controls();
+
+                Con_Close();
+            }
+            else
+            {
+                MessageBox.Show("Insert Valid Details !!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+        
+            Con_Open();
+
+            SqlCommand Cmd = new SqlCommand("Delete_Student", Con);
+
+            Cmd.CommandType = CommandType.StoredProcedure;
+
+            Cmd.Parameters.Add("@ID", SqlDbType.Int).Value = tb_Student_ID.Text;
+
+            Cmd.Connection = Con;
+
+            Cmd.ExecuteNonQuery();
+          
+            if (MessageBox.Show("Are you Sure you want to delete this record ??","Deleting",MessageBoxButtons.YesNo,MessageBoxIcon.Question) == DialogResult.Yes)
+	        {
+		        MessageBox.Show("Student Record Deleted Successfully !!!", "Saving", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                View_Student_Details();
+
+                Clear_Controls();
+
+                Con_Close();
+	        }
+            else
+            {
+                MessageBox.Show("Insert Valid Details !!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btn_Search_Click(object sender, EventArgs e)
+        {
+            SqlCommand Cmd = new SqlCommand("Search_Student", Con);
+
+            Cmd.CommandType = CommandType.StoredProcedure;
+
+            Cmd.Parameters.Add("@ID", SqlDbType.Int).Value = tb_Student_ID.Text;
+
+            Cmd.Connection = Con;
+
+            SqlDataAdapter SDA = new SqlDataAdapter(Cmd);
+
+            DataTable dt = new DataTable();
+
+            SDA.Fill(dt);
+
+            dgv_Crud_Application.DataSource = dt;
+
+            Clear_Controls();
+        }
+    }
+}
